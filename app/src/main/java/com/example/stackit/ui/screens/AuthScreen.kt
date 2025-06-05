72a0bceb225ac5b2734f352fed082fe75af69eb6
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2025-06-05 16:24:26 +0200

    auth screen ui done. no functionallity. only change between screens hardcoded

--- app/src/main/java/com/example/stackit/ui/screens/AuthScreen.kt
+++ app/src/main/java/com/example/stackit/ui/screens/AuthScreen.kt
@@ -4,33 +4,70 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.stackit.ui.theme.StackItTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.TextButton
+
 
 @Composable
 fun AuthScreen(onLoginClicked: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(text = "Authentication Screen", style = MaterialTheme.typography.headlineLarge)
-            Spacer(modifier = Modifier.height(16.dp)) // Add some space
-            Button(onClick = onLoginClicked) { // Call the lambda when button is clicked
-                Text("Simulate Login")
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onLoginClicked,
+                modifier = Modifier.fillMaxWidth()) { // Call the lambda when button is clicked
+                Text("Sign in")
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedButton(
+                onClick = onLoginClicked,
+                modifier = Modifier.fillMaxWidth()) { // should be changed by signup
+                Text("Sign up")
             }
         }
     }
